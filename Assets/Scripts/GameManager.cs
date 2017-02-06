39c692f0348cade0e1a3f2cad8c47284d4935d4c
Will Brewer <wbrewer6@gatech.edu>
2017-02-06 17:57:41 -0500

    First Push
    bleh

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+    public Text uiText;
+    public string inputText;
+
+	// Use this for initialization
+	void Start () {
+        inputText = "";
+        uiText.text = "Type to Start";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        foreach(char c in Input.inputString) {
+            inputText += c;
+        }
+
+        uiText.text = inputText;
+
+        if(Input.GetKeyDown(KeyCode.Return)) {
+            inputText = "";
+        }
+    }
+}
